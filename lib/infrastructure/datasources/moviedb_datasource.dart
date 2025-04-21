31cf9ce8b13de6fd3f8e982557e1b2eeaf3dfabc
Victor Alvarez <alvarez3197@gmail.com>
2025-04-21 16:01:47 -0400

    Se realizo el mapeo de la respuesta de moviedb

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,23 +1,30 @@
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
-  final dio = Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/3/movie',
-    queryParameters: {
-        'api_key': Environment.movieDbKey,
-        'language': 'es-MX'
-    }
-  ));
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3/movie',
+      queryParameters: {'api_key': Environment.movieDbKey, 'language': 'es-MX'},
+    ),
+  );
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-
     final response = await dio.get("/now_playing");
-    
-    final List<Movie> movies = [];
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies =
+        movieDBResponse.results
+            .where((moviedb) => moviedb.posterPath != 'no-poster')
+            .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+            .toList();
 
     return movies;
   }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -17,7 +17,7 @@ class MovieMapper {
     posterPath:
         (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-            : 'https://cdn.displate.com/artwork/270x380/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            : 'no-poster',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
