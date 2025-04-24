54bee1d3c180760103c9f757120dfbdc72c33a0b
Victor Alvarez <alvarez3197@gmail.com>
2025-04-24 13:30:56 -0400

    se creo el provider para el movies slideshow

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -30,10 +32,14 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final moviesSlideshow = ref.watch(moviesSlideshowProvider);
 
     return Column(
-      children: [CustomAppbar(), MoviesSlideshow(movies: nowPlayingMovies),],
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: moviesSlideshow),
+      ],
     );
   }
 }
