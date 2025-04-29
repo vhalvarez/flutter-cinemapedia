7b8f59a8970261b649d68cd3afe5bad4098b49f9
Victor Alvarez <alvarez3197@gmail.com>
2025-04-29 11:31:03 -0400

    se obtienen las peliculas por id y se creo que moviedetails entity

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -45,15 +46,36 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> getUpcoming({int page = 1}) async {
-    final response = await dio.get("/upcoming", queryParameters: {'page': page});
+    final response = await dio.get(
+      "/upcoming",
+      queryParameters: {'page': page},
+    );
 
     return _jsonToMovies(response.data);
   }
 
   @override
   Future<List<Movie>> getTopRated({int page = 1}) async {
-    final response = await dio.get("/top_rated", queryParameters: {'page': page});
+    final response = await dio.get(
+      "/top_rated",
+      queryParameters: {'page': page},
+    );
 
     return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get("/movie/$id");
+
+    if (response.statusCode != 200) {
+      throw Exception('Movie with id: $id not found');
+    }
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -17,11 +18,34 @@ class MovieMapper {
     posterPath:
         (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-            : 'no-poster',
+            : 'https://cdn.displate.com/artwork/270x380/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
     voteCount: moviedb.voteCount,
+  );
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+    adult: movie.adult,
+    backdropPath:
+        (movie.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movie.backdropPath}'
+            : 'https://cdn.displate.com/artwork/270x380/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+    genreIds: movie.genres.map((g) => g.name).toList(),
+    id: movie.id,
+    originalLanguage: movie.originalLanguage,
+    originalTitle: movie.originalTitle,
+    overview: movie.overview,
+    popularity: movie.popularity,
+    posterPath:
+        (movie.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movie.posterPath}'
+            : 'https://cdn.displate.com/artwork/270x380/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+    releaseDate: movie.releaseDate,
+    title: movie.title,
+    video: movie.video,
+    voteAverage: movie.voteAverage,
+    voteCount: movie.voteCount,
   );
 }
