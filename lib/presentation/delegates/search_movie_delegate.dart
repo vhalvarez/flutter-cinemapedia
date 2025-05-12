8e637506b1c489470ccb77ca2a6e06725d928908
Victor Alvarez <alvarez3197@gmail.com>
2025-05-12 12:18:02 -0400

    search delegate

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate{
+
+
+    @override
+  String get searchFieldLabel => 'Buscar pelicula...';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+        Text('buildActions'),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return const Text('buildLeading');
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text('buildSuggestions');
+  }
+
+}
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppbar extends StatelessWidget {
@@ -20,7 +21,11 @@ class CustomAppbar extends StatelessWidget {
               const SizedBox(width: 5),
               Text('Cinemapedia', style: titleStyle),
               Spacer(),
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              IconButton(onPressed: () {
+                showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate());
+              }, icon: Icon(Icons.search)),
             ],
           ),
         ),
